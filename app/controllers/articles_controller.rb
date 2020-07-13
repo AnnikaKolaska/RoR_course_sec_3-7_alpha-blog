0131735fc72ceb700cf31c92bd6d10a8e074e82f
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-13 12:15:19 +0200

    add create article with errormessages or flashmessage if successfull

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,22 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    # usefull: render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created sucessfully"
+      redirect_to article_path(@article) #automatically extracts the id form the article
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -21,4 +31,12 @@ class ArticlesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    # returns whitelisted parameters for an article
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 end
