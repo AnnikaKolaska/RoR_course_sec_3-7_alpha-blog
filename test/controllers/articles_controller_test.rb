e15e6dca4a950467cb9d735010e35d982ce18820
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-14 17:33:23 +0200

    fixed articles controller test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,39 +1,46 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = articles(:one)
+  end
+  
   test "should get index" do
-    get articles_index_url
+    get articles_path
     assert_response :success
   end
 
   test "should get new" do
-    get articles_new_url
+    get new_article_path
     assert_response :success
   end
 
-  test "should get create" do
-    get articles_create_url
-    assert_response :success
+  test "should create article" do
+    assert_difference('Article.count') do
+      post articles_path, params: { article: { title: 'new title', description: 'new description thats long enough' } }
+    end
+    assert_redirected_to article_path(Article.last)
   end
 
   test "should get show" do
-    get articles_show_url
+    get article_path(@article)
     assert_response :success
   end
 
   test "should get edit" do
-    get articles_edit_url
+    get edit_article_path(@article)
     assert_response :success
   end
 
-  test "should get update" do
-    get articles_update_url
-    assert_response :success
+  test "should update article" do
+    patch article_path(@article), params: { article: { title: @article.title } }
+    assert_redirected_to article_path(@article)
   end
 
   test "should get destroy" do
-    get articles_destroy_url
-    assert_response :success
+    assert_difference('Article.count', -1) do
+      delete article_path(@article)
+    end
   end
 
 end
