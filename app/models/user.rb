cb1ddad7b5b1996cb15ff6f5c4b9a991afabd3b8
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-15 10:31:15 +0200

    add bcrypt secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
                     length: {maximum: 105},
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
-
+  has_secure_password
+  
   private
     def email_to_lowercase
       self.email = email.downcase
