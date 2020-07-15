54c4bd5f352f9bfdbbfe47a797bdbe9d6890cd26
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-15 12:06:09 +0200

    add signup form template for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'about', to: 'pages#about'
+  get 'about',  to: 'pages#about'
   resources :articles 
-  resources :users
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
