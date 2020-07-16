ffaf632034ee19326704191c6b3c40a6f99dbe80
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-16 12:48:45 +0200

    bugfix: user/current loggedin user not switching anymore when clicking on the ownername of an article (thus being able to delete foreign articles)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # returns the currents user if one exists
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   # returns of there is a user logged in or not
