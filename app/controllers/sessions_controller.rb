f801112263e2755a19cee112eda1e32eb70b07f8
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-16 09:23:14 +0200

    add and fix user login (without session for now)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # create session
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "E-mail or password is wrong"
+      render 'new'
+    end
   end
 
   def destroy
