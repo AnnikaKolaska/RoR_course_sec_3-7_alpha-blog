c55fd1c52a90b6e1af48d45fe1d91b9fffcdcc96
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-16 14:06:36 +0200

    restrict actions for articles on controller level

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_logged_in_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -52,5 +54,12 @@ class ArticlesController < ApplicationController
     # Returns whitelisted parameters for an article
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:alert] = "You can only edit or delete you own article!"
+        redirect_to @article
+      end
     end
 end
