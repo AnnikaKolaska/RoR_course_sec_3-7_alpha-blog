35c82ffee869c47e176617537d513da41f1abf74
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-16 08:02:03 +0200

    add sessionscontroller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pages#home'
   get 'about',  to: 'pages#about'
   resources :articles 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'sessions#destroy'
 end
