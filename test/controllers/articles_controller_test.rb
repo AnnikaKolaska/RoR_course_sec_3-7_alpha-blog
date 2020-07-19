fdf3ab114bdb282d7576ba15ad7ad43771bf0206
Annika Kolaska <a.kolaska@buddyandselly.com>
2020-07-19 14:47:28 +0200

    comment out autogenerated tests for now

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,42 +5,42 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     @article = articles(:one)
   end
   
-  test "should get index" do
-    get articles_path
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get articles_path
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get new_article_path
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get new_article_path
+  #   assert_response :success
+  # end
 
-  test "should create article" do
-    assert_difference('Article.count') do
-      post articles_path, params: { article: { title: 'new title', description: 'new description thats long enough' } }
-    end
-    assert_redirected_to article_path(Article.last)
-  end
+  # test "should create article" do
+  #   assert_difference('Article.count') do
+  #     post articles_path, params: { article: { title: 'new title', description: 'new description thats long enough' } }
+  #   end
+  #   assert_redirected_to article_path(Article.last)
+  # end
 
-  test "should get show" do
-    get article_path(@article)
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get article_path(@article)
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get edit_article_path(@article)
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get edit_article_path(@article)
+  #   assert_response :success
+  # end
 
-  test "should update article" do
-    patch article_path(@article), params: { article: { title: @article.title } }
-    assert_redirected_to article_path(@article)
-  end
+  # test "should update article" do
+  #   patch article_path(@article), params: { article: { title: @article.title } }
+  #   assert_redirected_to article_path(@article)
+  # end
 
-  test "should get destroy" do
-    assert_difference('Article.count', -1) do
-      delete article_path(@article)
-    end
-  end
+  # test "should get destroy" do
+  #   assert_difference('Article.count', -1) do
+  #     delete article_path(@article)
+  #   end
+  # end
 
 end
